3c135b182c137e37328883bfedab9f4b9cc49f2e
Noritaka Horio <holy.shared.design@gmail.com>
2011-05-28 18:38:02 +0900

    update.
    Signed-off-by: Noritaka Horio <holy.shared.design@gmail.com>

--- js/runner.js
+++ js/runner.js
@@ -4,14 +4,41 @@
 
 	window.addEvent('domready', function(){
 
-		$('attach').addEvent('click', function(event){
+		var selecter = '#attachEvents a';
+
+		var attach = $('attach');
+		var dettach = $('dettach');
+		var elements = $$(selecter);
+
+		var dettachHandler = function(event){
 			event.preventDefault();
-			mock.attach('#attachEvents a');
+		};
+
+		mock.attach(selecter);
+		attach.addClass('disable');
+
+		attach.addEvent('click', function(event){
+			event.preventDefault();
+			if (mock.isAttached(selecter)) return;
+			mock.attach(selecter);
+
+			this.addClass('disable');
+			dettach.removeClass('disable')
+				.addClass('enable');
+
+			elements.removeEvent('click', dettachHandler);
 		});
 
-		$('dettach').addEvent('click', function(event){
+		dettach.addEvent('click', function(event){
 			event.preventDefault();
-			mock.dettach('#attachEvents a');
+			if (!mock.isAttached(selecter)) return;
+			mock.dettach(selecter);
+
+			this.addClass('disable');
+			attach.removeClass('disable')
+				.addClass('enable');
+
+			elements.addEvent('click', dettachHandler);
 		});
 
 	});
